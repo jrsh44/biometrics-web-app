8bcfc643dc3a0ffd17d5d33c60732253df5ee6a9
jrsh44 <mateuszjarosz44@gmail.com>
2025-03-12 13:56:55 +0100

    Format manipulate functions

--- src/utils/manipulate.ts
+++ src/utils/manipulate.ts
@@ -8,33 +8,32 @@ export const applyBrightness = (
   g: number,
   b: number,
   brightness: number,
-): [number, number, number] => {
-  return [r * (brightness / 100), g * (brightness / 100), b * (brightness / 100)];
-};
+): [number, number, number] => [
+  r * (brightness / 100),
+  g * (brightness / 100),
+  b * (brightness / 100),
+];
 
 export const applyContrast = (
   r: number,
   g: number,
   b: number,
   contrast: number,
-): [number, number, number] => {
-  return [
-    (r - 128) * (contrast / 100) + 128,
-    (g - 128) * (contrast / 100) + 128,
-    (b - 128) * (contrast / 100) + 128,
-  ];
-};
+): [number, number, number] => [
+  (r - 128) * (contrast / 100) + 128,
+  (g - 128) * (contrast / 100) + 128,
+  (b - 128) * (contrast / 100) + 128,
+];
 
-export const applyNegative = (r: number, g: number, b: number): [number, number, number] => {
-  return [255 - r, 255 - g, 255 - b];
-};
+export const applyNegative = (r: number, g: number, b: number): [number, number, number] => [
+  255 - r,
+  255 - g,
+  255 - b,
+];
 
 export const applyBinarization = (
   r: number,
   g: number,
   b: number,
   threshold: number,
-): [number, number, number] => {
-  const avg = (r + g + b) / 3;
-  return avg < threshold ? [0, 0, 0] : [255, 255, 255];
-};
+): [number, number, number] => ((r + g + b) / 3 < threshold ? [0, 0, 0] : [255, 255, 255]);
